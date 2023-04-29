139b4a560c47e50eca7a36c5bfa7a7033e9c12f3
Marcus-Friis <marcus.friis99@gmail.com>
2023-04-29 12:57:06 +0200

    Ml flow setup

--- src/torch_utilities.py
+++ src/torch_utilities.py
@@ -6,14 +6,18 @@ import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-import wandb
+import mlflow
 
 from IPython.display import HTML
 import matplotlib.animation as animation
 
 
-def generate_grid(generator, latent_dim=100, n=10, device='cpu'):
-    noise = torch.randn(n*n, latent_dim).to(device)
+def generate_grid(generator, noise=None, latent_dim=100, n=10, device='cpu', show=True):
+    if noise is None:
+        noise = torch.randn(n*n, latent_dim).to(device)
+    else:
+        assert noise.shape[0] == n*n, "noise must be of shape (n*n, latent_dim)"
+        noise[:n]
     x_hat = generator(noise)
     x_hat = x_hat.view(-1, 28, 28)
     x_hat = x_hat.detach().cpu().numpy()
@@ -23,21 +27,31 @@ def generate_grid(generator, latent_dim=100, n=10, device='cpu'):
         for j in range(n):
             ax[i, j].imshow(x_hat[:, :, i*n + j], cmap='gray')
             ax[i, j].axis('off')
-    plt.show()
+    if show:
+        plt.show()
     return fig, ax
 
 
-def training_animation(img_list, interval=1000, repeat_delay=1000):
+def training_animation(img_list, interval=1000, repeat_delay=1000, show=True):
     fig = plt.figure(figsize=(8,8))
     plt.axis("off")
     ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
     ani = animation.ArtistAnimation(fig, ims, interval=interval, repeat_delay=repeat_delay, blit=True)
-
-    HTML(ani.to_jshtml())
+    
+    if show:
+        HTML(ani.to_jshtml())
+        
     return fig, ani
 
 
-# write boilerplate code using wandb for logging a models performance
-def wandb_init(project_name, model, config, notes=None):
-    wandb.init(project=project_name, config=config, notes=notes)
-    wandb.watch(model)
+def plot_gan_loss(G_losses, D_losses, show=True):
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.set_title("Generator and Discriminator Loss During Training")
+    ax.plot(G_losses,label="G")
+    ax.plot(D_losses,label="D")
+    ax.set_xlabel("iterations")
+    ax.set_ylabel("Loss")
+    ax.legend()
+    if show:
+        plt.show()
+    return fig, ax
