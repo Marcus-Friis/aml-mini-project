a31067b4587b8834038ff9c00b44a0e8be1b1b51
Marcus Friis <mahf@itu.dk>
2023-05-08 11:13:25 +0200

    prints device

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
     # setup device
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
+    print('using device: ', device)
 
     # hyperparameters
     batch_size = 64
